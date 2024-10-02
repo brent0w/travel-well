84f899dba4d947355130bf96ab0953588038ed4d
Brent <brentdwaller@gmail.com>
2024-10-02 12:10:32 -0700

    Added search functionality and country images

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -8,29 +8,48 @@ function fetchDestinations() {
     fetch('./travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
-            const destination = filterData(data);
-            console.log(destination);
-            if (destination) {
-                destination.forEach(element => {
-                    const name = element.name;
-                    const image = element.image;
-                    const description = element.description;
-                    console.log(name);
+            const query = searchInput.value.toLowerCase();
+            const type = matchSearch(query);
+            console.log(type); // TODO: Remove when this is working properly
+            const destination = data[type];
+            if (type === "countries") {
+                destination.forEach(country => {
+                    const countryName = country.name;
+                    resultsDiv.innerHTML += `<h2>${countryName}</h2>`;
+                    console.log(countryName);
+                    country.cities.forEach(city => {
+                        const cityName = city.name;
+                        const imageUrl = city.imageUrl;
+                        const description = city.description;
+                        resultsDiv.innerHTML += `<h3>${cityName}</h3>`;
+                        resultsDiv.innerHTML += `<img src="${imageUrl}" alt="Photo of ${cityName}">`
+                        resultsDiv.innerHTML += `<p>${description}</p>`;
+                        console.log(cityName);
+                        console.log(description);
+                    });
+                });
+            } else if (type) {
+                destination.forEach(city => {
+                    const cityName = city.name;
+                    const imageUrl = city.imageUrl;
+                    const description = city.description;
+                    console.log(cityName);
+                    console.log(description);
                 });
             } else {
-                resultsDiv.innerHTML = "No results found.";
+                resultsDiv.innerHTML = `<p>No results found for "${query}", but we know there is destination for you! \
+                <br> How about searching for beaches, temples, or countries?</p>`;
             }
         })
     }
 
-function filterData(data) {
-    const query = searchInput.value.toLowerCase();
+function matchSearch(query) {
     if (query.search("countr") > -1) {
-        return data.countries;
+        return "countries";
     } else if (query.search("temple") > -1) {
-        return data.temples;
+        return "temples";
     } else if (query.search("beach") > -1) {
-        return data.beaches;
+        return "beaches";
     } else {
         return false;
     }
